e809bb837ef87baa5c21c40cc1e4afce7ca86211
Roman SHamagin <roman.shamagin@7vpn.com>
2024-05-25 22:35:55 +0300

    feat: update lang

---
+++ translations/en/admin.php
@@ -0,0 +1,34 @@
+<?php
+
+global $_LANGADM;
+$_LANGADM = array();
+$_LANGADM['AdminAddresses03361eda68f746619c2ae3341eaa2f07'] = 'Customer email';
+$_LANGADM['AdminAddresses1c76cbfe21c6f44c1d1e59d54f3e4420'] = 'Company';
+$_LANGADM['AdminAddresses284b47b0bb63ae2df3b29f0e691d6fcf'] = 'Addresses';
+$_LANGADM['AdminAddresses3e053943605d9e4bf7dd7588ea19e9d2'] = 'Forbidden characters:';
+$_LANGADM['AdminAddresses41c2fff4867cc204120f001e7af20f7a'] = 'Mobile phone';
+$_LANGADM['AdminAddresses46a2a41cc6e552044816a2d04634545d'] = 'State';
+$_LANGADM['AdminAddresses57d056ed0984166336b7879c2af3657f'] = 'City';
+$_LANGADM['AdminAddresses59716c97497eb9694541f7c3d37b1a4d'] = 'Country';
+$_LANGADM['AdminAddresses6252c0f2c2ed83b7b06dfca86d4650bb'] = 'Invalid characters:';
+$_LANGADM['AdminAddresses6311ae17c1ee52b36e68aaf4ad066387'] = 'Other';
+$_LANGADM['AdminAddresses72d6d7a1885885bb55a565fd1070581a'] = 'Import';
+$_LANGADM['AdminAddresses77587239bf4c54ea493c7033e1dbf636'] = 'Last Name';
+$_LANGADM['AdminAddresses7cb32e708d6b961d476baced73d362bb'] = 'VAT number';
+$_LANGADM['AdminAddresses919d1ffe6c1855e790a416efa7b4cc4e'] = 'Identification Number	';
+$_LANGADM['AdminAddresses9ea67be453eaccf020697b4654fc021a'] = 'Save and stay';
+$_LANGADM['AdminAddressesb718adec73e04ce3ec720dd11a06a308'] = 'ID';
+$_LANGADM['AdminAddressesbaa31a65f29121c32b637bb845d41acf'] = 'Address alias';
+$_LANGADM['AdminAddressesbc910f8bdf70f29374f496f05be0330c'] = 'First Name';
+$_LANGADM['AdminAddressesbed08e8af70a98c1a8361f13ec477be0'] = 'Add new address';
+$_LANGADM['AdminAddressesc9cc8cce247e49bae79f15173ce97354'] = 'Save';
+$_LANGADM['AdminAddressesce26601dac0dea138b7295f02b7620a7'] = 'Customer';
+$_LANGADM['AdminAddressesd3b206d196cd6be3a2764c1fb90b200f'] = 'Delete selected';
+$_LANGADM['AdminAddressesdd7bf230fde8d4836917806aff6a6b27'] = 'Address';
+$_LANGADM['AdminAddressese25f0ecd41211b01c83e5fec41df4fe7'] = 'Delete selected items?';
+$_LANGADM['AdminAddressese4eb5dadb6ee84c5c55a8edf53f6e554'] = 'Zip/Postal Code';
+$_LANGADM['AdminAddressesea318a4ad37f0c2d2c368e6c958ed551'] = 'DNI / NIF / NIE';
+$_LANGADM['AdminAddresseseeabead01c6c6f25f22bf0b041df58a9'] = 'You must register at least one phone number.';
+$_LANGADM['AdminAddressesfe66abce284ec8589e7d791185b5c442'] = 'Home phone	';
+
+?>
