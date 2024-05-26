2629eb40e761e3df96fedf695f6ce14f3ac57a5c
Google Cloud Translation Advanced <noreply-mt-google-translate-api-v3@weblate.org>
2024-05-26 00:18:38 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: Danang.rent/hotelreservationsystem
    Translate-URL: https://translate.7vpn.com/projects/danang-rent/hotelreservationsystem/ko/

--- modules/hotelreservationsystem/translations/ko.php
+++ modules/hotelreservationsystem/translations/ko.php
@@ -1,2 +1,2 @@
 <?php
-$_MODULE['<{hotelreservationsystem}prestashop>headerhoteldescblock_1c9b5c29d6c1d0ab2595b793cf1ddfd7'] = '';
+$_MODULE['<{hotelreservationsystem}prestashop>headerhoteldescblock_1c9b5c29d6c1d0ab2595b793cf1ddfd7'] = '에 오신 것을 환영합니다';
